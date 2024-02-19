7d3780b3d115ce82983fe6b7d66c077398df48cf
dkrupkina <dariakrupkina@gmail.com>
2024-02-19 18:53:43 +0100

    [TW-85889] Attack with ktor-network-jvm and udp

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -67,12 +67,12 @@ object Build : BuildType({
 
     runBlocking {
         val selectorManager = SelectorManager(Dispatchers.IO)
-        val serverSocket = aSocket(selectorManager).udp().
-                bind(io.ktor.network.sockets.InetSocketAddress("127.0.0.1", 8171))
+        val serverSocket = aSocket(selectorManager).udp().connect(io.ktor.network.sockets.InetSocketAddress("127.0.0.1", 8171))
 
-        serverSocket.send(Datagram(ByteReadPacket("You're attacked!".encodeToByteArray()), serverSocket.localAddress))
+        serverSocket.openWriteChannel(autoFlush = true).writeStringUtf8("You're attacked!")
         withContext(Dispatchers.IO) {
             serverSocket.close()
+            selectorManager.close()
         }
     }
 
