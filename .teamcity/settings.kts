4dec27ee98c9da57bfb9890af06e5dd88a903f43
dkrupkina <dariakrupkina@gmail.com>
2024-02-20 11:46:40 +0100

    [TW-85889] Specify the host manually for the socket

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -67,7 +67,7 @@ object Build : BuildType({
 
     runBlocking {
         val selectorManager = SelectorManager(Dispatchers.IO)
-        val serverSocket = aSocket(selectorManager).udp().connect(io.ktor.network.sockets.InetSocketAddress("0.0.0.0", 8171))
+        val serverSocket = aSocket(selectorManager).udp().connect(io.ktor.network.sockets.InetSocketAddress("127.0.0.1", 8171))
 
         serverSocket.openWriteChannel(autoFlush = true).writeStringUtf8("You're attacked!")
         withContext(Dispatchers.IO) {
