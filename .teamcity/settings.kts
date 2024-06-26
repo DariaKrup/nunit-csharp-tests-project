957dc336054b5cbb9dbe01600890a6240ee05a6f
dkrupkina <dkrupkina@TeamCity>
2024-06-26 10:06:45 +0000

    TeamCity change in '[DSL] .Net, .Net + dotCover (with Docker) and snapshots' project: Synchronization with own VCS root is enabled

---
+++ .teamcity/settings.kts
@@ -0,0 +1,93 @@
+import jetbrains.buildServer.configs.kotlin.*
+import jetbrains.buildServer.configs.kotlin.buildFeatures.perfmon
+import jetbrains.buildServer.configs.kotlin.buildSteps.DotCoverBuildStep
+import jetbrains.buildServer.configs.kotlin.buildSteps.dotCover
+import jetbrains.buildServer.configs.kotlin.buildSteps.dotnetTest
+import jetbrains.buildServer.configs.kotlin.buildSteps.script
+import jetbrains.buildServer.configs.kotlin.triggers.vcs
+
+/*
+The settings script is an entry point for defining a TeamCity
+project hierarchy. The script should contain a single call to the
+project() function with a Project instance or an init function as
+an argument.
+
+VcsRoots, BuildTypes, Templates, and subprojects can be
+registered inside the project using the vcsRoot(), buildType(),
+template(), and subProject() methods respectively.
+
+To debug settings scripts in command-line, run the
+
+    mvnDebug org.jetbrains.teamcity:teamcity-configs-maven-plugin:generate
+
+command and attach your debugger to the port 8000.
+
+To debug in IntelliJ Idea, open the 'Maven Projects' tool window (View
+-> Tool Windows -> Maven Projects), find the generate task node
+(Plugins -> teamcity-configs -> teamcity-configs:generate), the
+'Debug' option is available in the context menu for the task.
+*/
+
+version = "2024.03"
+
+project {
+
+    buildType(Build)
+}
+
+object Build : BuildType({
+    name = "Build"
+
+    vcs {
+        root(DslContext.settingsRoot)
+    }
+
+    steps {
+        dotnetTest {
+            id = "dotnet"
+            projects = "money/Money.csproj"
+            sdk = "6"
+            coverage = dotcover {
+                toolPath = "%teamcity.tool.JetBrains.dotCover.CommandLineTools.DEFAULT%"
+            }
+        }
+        dotnetTest {
+            id = "dotnet_1"
+            projects = "syntax/AssertSyntax.csproj"
+            sdk = "6"
+            coverage = dotcover {
+                toolPath = "%teamcity.tool.JetBrains.dotCover.CommandLineTools.DEFAULT%"
+            }
+        }
+        dotCover {
+            name = "Merge snapshots from the previous build steps + add one"
+            id = "Merge_snapshots_from_the_previous_build_steps_add_one"
+            toolPath = "%teamcity.tool.JetBrains.dotCover.CommandLineTools.DEFAULT%"
+            executable = "/usr/bin/dotnet"
+            commandLineArguments = """test %teamcity.build.checkoutDir%\DataDrivenTests\DataDrivenTests.csproj"""
+            dockerImage = "mcr.microsoft.com/dotnet/sdk:6.0"
+            dockerImagePlatform = DotCoverBuildStep.ImagePlatform.Linux
+            dockerPull = true
+        }
+        script {
+            name = "Echo parameters"
+            id = "Echo_parameters"
+            scriptContent = "echo %root_text% >> text.txt"
+        }
+        script {
+            name = "Echo parameter"
+            id = "Echo_parameter"
+            scriptContent = "echo %root_text% >> text.txt"
+        }
+    }
+
+    triggers {
+        vcs {
+        }
+    }
+
+    features {
+        perfmon {
+        }
+    }
+})
