4c492a1f90e3eedeb83b8f90af69f335ce6f298c
Daria Krupkina <55836255+DariaKrup@users.noreply.github.com>
2025-03-11 17:24:15 +0100

    Update settings.kts

--- .teamcity_k8s_params/settings.kts
+++ .teamcity_k8s_params/settings.kts
@@ -57,7 +57,7 @@ project {
             id = "PROJECT_EXT_172"
             connectionId = "PROJECT_EXT_2"
             profileName = "K8S"
-            containerParameters = "k8s=true,a=a"
+            containerParameters = "k8s=true,teamcity.agent.jvm.os.name=Linux"
             templateName = "ubuntu-agent"
         }
     }
