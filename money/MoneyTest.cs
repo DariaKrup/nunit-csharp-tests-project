5b719b48ed56e74fd6603de0236dc79026d28f12
Daria Krupkina <55836255+DariaKrup@users.noreply.github.com>
2026-02-13 12:20:01 +0100

    Update MoneyTest.cs: one more negative scenario

--- money/MoneyTest.cs
+++ money/MoneyTest.cs
@@ -189,6 +189,19 @@ namespace Money
             ClassicAssert.IsFalse(f12CHF.Equals(f14CHF));
         }
 
+		[Test]
+        public void MoneyEqualsMore()
+        {
+            //NOTE: Normally we use Assert.AreEqual to test whether two
+            // objects are equal. But here we are testing the MoneyBag.Equals()
+            // method itself, so using AreEqual would not serve the purpose.
+            ClassicAssert.IsFalse(f12CHF.Equals(null));
+            var equalMoney = new Money(12, "CHF");
+            ClassicAssert.IsTrue(f12CHF.Equals(f16CHF));
+            ClassicAssert.IsTrue(f12CHF.Equals(equalMoney));
+            ClassicAssert.IsFalse(f12CHF.Equals(f14CHF));
+        }
+
         /// <summary>
         /// Assert that the hash of new Money is the same as 
         /// the hash of initialized Money
@@ -329,4 +342,5 @@ namespace Money
         }
     }
 }
+
 
